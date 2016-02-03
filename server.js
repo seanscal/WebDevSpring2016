ed0f799ad3af255a1781750d0b3574f38241c9e9
Sean Scalabrini <sscalabrini@digitallumens.com>
2016-02-03 14:18:17 -0500

    finished assignment 1

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-
+/**
+ * Created by AliHyder on 1/17/16.
+ */
 var express = require('express');
 var app = express();
+
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-app.get('/hello', function(req, res){
-    res.send('hello world');
-});
+
+app.use(express.static(__dirname + '/public'));
+
 app.listen(port, ipaddress);
