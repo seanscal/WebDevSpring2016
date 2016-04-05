a89968209ed6b4ca32cc0390258b75b77726599d
Sean Scalabrini <sscalabrini@digitallumens.com>
2016-04-05 12:59:34 -0400

    502

--- public/project/server/models/game.model.js
+++ public/project/server/models/game.model.js
@@ -110,6 +110,7 @@ module.exports = function (mongoose, db) {
         console.log("In the model.");
         findGameById(gameId).then(function (game) {
             console.log("found the game");
+            console.log(game.stats[0].goalSummary)
             for (var x in game.stats[0].goalSummary) {
                 console.log("game video" + game.stats[0].goalSummary[x].goalId);
                 console.log("video: "+ videoId);
