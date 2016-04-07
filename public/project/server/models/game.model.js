01a0e45f0d5a589a9c14f246c613b6081a63eeb5
Sean Scalabrini <sscalabrini@digitallumens.com>
2016-04-06 20:47:33 -0400

    delete not working?

--- public/project/server/models/game.model.js
+++ public/project/server/models/game.model.js
@@ -182,10 +182,7 @@ module.exports = function (mongoose, db) {
                 game.stats = correctedStats;
                 console.log(game._id);
                 delete game._id;
-                console.log(game._id);
-                console.log(game.stats);
-                delete game.stats._id;
-                console.log(game.stats);
+                console.log("After delete" + game._id);
                 Game.update({gameId: gameId}, game, function (err, response) {
                     console.log(err);
                     findGameById(gameId).then(function (game) {
